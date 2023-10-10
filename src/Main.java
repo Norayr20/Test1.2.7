3d046720e5892c1fd5c3ec3b041114e00c718812
Norayr <asoyannorayr99@gmail.com>
2023-10-10 19:06:10 +0300

    add commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double price = 10002;
+        if (price > 1000)
+            price = price * 0.9;
+        System.out.println("Стоимость вашей покупки " + price + " рублей");
+    }
+}
